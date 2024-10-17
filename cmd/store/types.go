9b813cc41e6f9a14f771662a7ed91d20e9bc1a28
johnotieno0 <poptiff@gmail.com>
2024-10-17 15:52:34 +0300

    refactor: Cardetails struct Details field to take a slice of maps

--- cmd/store/types.go
+++ cmd/store/types.go
@@ -3,10 +3,10 @@ package store
 // Struct to hold car page details
 type CarPageDetails struct {
     Title       string `json:"title"`
-    Details     []Detail `json:"details"`
+    Details     []map[string]string `json:"details"`
 }
 
-type Detail struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
-}
+// type Detail struct {
+// 	Key   string `json:"key"`
+// 	Value string `json:"value"`
+// }
